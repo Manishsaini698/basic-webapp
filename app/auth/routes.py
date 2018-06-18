e6e723fd382731de91b88375c9109f835e959ac3
mani <manishsaini698@gmail.com>
2018-06-18 21:28:30 +0530

    yoyo

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -28,7 +28,7 @@ def login():
 @bp.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('auth.index'))
+    return redirect(url_for('main.index'))
 
 
 @bp.route('/register', methods=['GET', 'POST'])

c74145acae946f7464a45498feffc270ffbfce9e
mani <manishsaini698@gmail.com>
2018-06-18 21:24:27 +0530

    yoyo

--- app/main/routes.py
+++ app/main/routes.py
@@ -22,8 +22,7 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user,
-                    language=language)
+        post = Post(body=form.post.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')

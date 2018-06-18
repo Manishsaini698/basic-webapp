bd5cacd3fe1a547319874feaf7cd0edf7110b127
mani <manishsaini698@gmail.com>
2018-06-18 21:17:39 +0530

    yoyo

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -9,22 +9,6 @@ from app import db
 from werkzeug.urls import url_parse
 from app.email import send_password_reset_email
 
-@bp.route('/', methods=['GET', 'POST'])
-@bp.route('/index', methods=['GET', 'POST'])
-@login_required
-def index():
-    form = PostForm()
-    if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
-        db.session.add(post)
-        db.session.commit()
-        flash('Your post is now live!')
-        return redirect(url_for('auth.index'))
-    page = request.args.get('page', 1, type=int)
-    posts = current_user.followed_posts().paginate(
-        page, app.config['POSTS_PER_PAGE'], False)
-    return render_template('index.html', title='Home', form=form,
-                           posts=posts.items)
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():

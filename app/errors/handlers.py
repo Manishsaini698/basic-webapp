c138715fc76e146cfd276088fc2240c2ae5e7296
mani <manishsaini698@gmail.com>
2018-06-19 06:25:02 +0530

    csrf added

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,5 +1,21 @@
-from flask import Blueprint
 
-bp = Blueprint('errors', __name__)
+from flask import render_template
+from app import db
+from app.errors import bp
+from flask_wtf.csrf import CSRFError
 
-from app.errors import handlers
+
+@bp.app_errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+
+@bp.app_errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('errors/500.html'), 500
+
+
+@bp.errorhandler(CSRFError)
+def handle_csrf_error(e):
+    return render_template('csrf_error.html', reason=e.description), 400
